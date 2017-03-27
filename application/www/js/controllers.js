1caaf96f524dff9d28870cc58f5bbfc6dc6ad0f9
qing yu <qyu37@wisc.edu>
2017-03-27 14:38:51 -0500

    show the group signup code

--- application/www/js/controllers.js
+++ application/www/js/controllers.js
@@ -1,68 +1,69 @@
 var starter = angular.module('starter.controllers', []);
 /*
-starter.controller('LoginCtrl', function($scope, $state)
-{
-    $scope.toHomeState = function ()
-    {
-        $state.go("tab.dash");
-    };
-  $scope.toLoginState = function ()
-  {
-    $state.go("tab.login");
+ starter.controller('LoginCtrl', function($scope, $state)
+ {
+ $scope.toHomeState = function ()
+ {
+ $state.go("tab.dash");
+ };
+ $scope.toLoginState = function ()
+ {
+ $state.go("tab.login");
+ };
+ })
+ */
+
+starter.controller('LoginCtrl', function($scope, $ionicLoading, $state) {
+  // $scope.user = {};
+  console.log("here")
+  $scope.email = "kitten@petbnb.com";
+  $scope.password = "123456";
+
+  // TODO: signin function - done
+  $scope.createUser = function(email, password) {
+    return firebase.auth().createUserWithEmailAndPassword(email,password).then(function(){
+      $ionicLoading.show({template:'Created New User!',noBackdrop:true,duration:1000});
+    }).catch(function(error){
+      var errorCode=error.code;
+      var errorMessage=error.message;
+      $ionicLoading.show({template:'Fail to Create New User! Try again!',noBackdrop:true,duration:1000});
+    });
   };
-})
-*/
 
- starter.controller('LoginCtrl', function($scope, $ionicLoading, $state) {
-    // $scope.user = {};
-    $scope.email = "kitten@petbnb.com";
-    $scope.password = "123456";
-
-    // TODO: signin function - done
-    $scope.createUser = function(email, password) {
-      return firebase.auth().createUserWithEmailAndPassword(email,password).then(function(){
-        $ionicLoading.show({template:'Created New User!',noBackdrop:true,duration:1000});
-      }).catch(function(error){
-        var errorCode=error.code;
-        var errorMessage=error.message;
-        $ionicLoading.show({template:'Fail to Create New User! Try again!',noBackdrop:true,duration:1000});
+  // TODO: login function - not yet
+  $scope.login = function(email, password) {
+    return firebase.auth.sigInWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
       });
-    };
+  };
 
-    // TODO: login function - not yet
-    $scope.login = function(email, password) {
-      return firebase.auth.sigInWithEmailAndPassword(email, password)
-        .catch(function(error) {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-        });
-    };
+  // TODO: attempt create user - done
+  $scope.attemptCreateUser = function(){
+    $scope.createUser($scope.email,$scope.password);
+  };
+  // TODO: attempt login - not yet
+  $scope.attemptLogin = function(){
+    console.log("Attempting Login with Email:"+$scope.email+"|password:"+$scope.password);
 
-    // TODO: attempt create user - done
-    $scope.attemptCreateUser = function(){
-      $scope.createUser($scope.email,$scope.password);
-    };
-    // TODO: attempt login - not yet
-    $scope.attemptLogin = function(){
-      console.log("Attempting Login with Email:"+$scope.email+"|password:"+$scope.password);
-
-      $scope.login($scope.email,$scope.password).then(function(){
-        //CheckifcurrentUserisset(weweresuccesfullyabletologin)
-        if(!firebase.auth().currentUser){
-          //Showmodalwithdescriptionofevents
-          $ionicLoading.show({template:'Fail to Login! Check credentials,check connection or Create user',noBackdrop:true, duration:1000});
-          // TODO: unsuccessful login
-        } else{
-          //If successful login,thencurrentUserissetanddisplayeventmodal
-          //Showmodalwithdescriptionofevents
-          $ionicLoading.show({template:'Successfully Login with Existing User!',noBackdrop:true,duration:1000});
-          // TODO: successful login
-          // $scope.logoutButton.username=firebase.auth().currentUser.email;
-          // $scope.logoutButton.visibility='visible';
-        }
-      });
-    };
-  })
+    $scope.login($scope.email,$scope.password).then(function(){
+      //CheckifcurrentUserisset(weweresuccesfullyabletologin)
+      if(!firebase.auth().currentUser){
+        //Showmodalwithdescriptionofevents
+        $ionicLoading.show({template:'Fail to Login! Check credentials,check connection or Create user',noBackdrop:true, duration:1000});
+        // TODO: unsuccessful login
+      } else{
+        //If successful login,thencurrentUserissetanddisplayeventmodal
+        //Showmodalwithdescriptionofevents
+        $ionicLoading.show({template:'Successfully Login with Existing User!',noBackdrop:true,duration:1000});
+        // TODO: successful login
+        // $scope.logoutButton.username=firebase.auth().currentUser.email;
+        // $scope.logoutButton.visibility='visible';
+      }
+    });
+  };
+});
 
 
 
@@ -84,7 +85,7 @@ starter.controller('DashCtrl', function($scope, $state)
 
     $state.go("tab.chats");
   };
-})
+});
 
 starter.controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
@@ -99,32 +100,27 @@ starter.controller('ChatsCtrl', function($scope, Chats) {
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
-})
+});
 
 starter.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
-})
+});
 
 starter.controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
-})
+});
 
 starter.controller('SearchCtrl', function($scope) {
-    $scope.settings = {
-      enableFriends: true
-    };
-})
+  $scope.settings = {
+    enableFriends: true
+  };
+});
 
 
 starter.controller('BlogCtrl', function($scope) {
-    $scope.settings = {
-      enableFriends: true
-    };
-})
-
-
-
-
-
+  $scope.settings = {
+    enableFriends: true
+  };
+});
